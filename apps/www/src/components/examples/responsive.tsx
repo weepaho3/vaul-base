0f0e9425995c5d19924fd0560e8e7d60569aee67
steffen.schlabbach <steffen.schlabbach@brandpfeil.de>
2026-01-03 10:45:06 +0100

    chore: update responsive drawer to use data attributes for styling

--- apps/www/src/components/examples/responsive.tsx
+++ apps/www/src/components/examples/responsive.tsx
@@ -2,7 +2,6 @@ import { Drawer } from "vaul-base"
 
 import { Button } from "@/components/button"
 import { useIsMobile } from "@/hooks/use-mobile"
-import { cn } from "@/lib/utils"
 
 const ResponsiveDrawer = () => {
   const isMobile = useIsMobile()
@@ -13,8 +12,18 @@ const ResponsiveDrawer = () => {
       />
       <Drawer.Portal>
         <Drawer.Overlay className="fixed inset-0 bg-black/80" />
-        <Drawer.Content className={cn("bg-background text-foreground fixed inset-x-0 bottom-0 h-auto w-full max-w-xl rounded-lg border", isMobile && "max-w-full rounded-b-none")}>
-         {isMobile && <Drawer.Handle className="top-4" />}
+        <Drawer.Content className="bg-background text-foreground fixed inset-x-0 bottom-0 h-auto w-full border group
+
+        data-[vaul-drawer-direction=center]:max-w-xl data-[vaul-drawer-direction=center]:rounded-lg
+        data-[vaul-drawer-direction=bottom]:max-w-full data-[vaul-drawer-direction=bottom]:rounded-b-none
+        data-[vaul-drawer-direction=top]:max-w-full data-[vaul-drawer-direction=top]:rounded-b-none
+        data-[vaul-drawer-direction=left]:max-w-64 data-[vaul-drawer-direction=left]:w-2/3 data-[vaul-drawer-direction=left]:rounded-lg data-[vaul-drawer-direction=left]:h-full
+        data-[vaul-drawer-direction=right]:max-w-64 data-[vaul-drawer-direction=right]:w-2/3 data-[vaul-drawer-direction=right]:rounded-lg data-[vaul-drawer-direction=right]:h-full
+        ">
+          <div className="hidden group-data-[vaul-drawer-direction=bottom]:block">
+            <Drawer.Handle className="top-3" />
+          </div>
+
           <div className="flex flex-col space-y-4 p-6">
             <h4 className="font-semibold">Welcome to the Drawer</h4>
             <p>
@@ -23,7 +32,7 @@ const ResponsiveDrawer = () => {
             <p>
               We just change the direction prop dynamically using a custom hook. The transition between directions is 
               handeled smoothly by the drawer component.
-            </p>
+            </p> 
 
 
             <pre className="rounded bg-muted p-4 text-sm overflow-x-auto">
@@ -32,7 +41,10 @@ const ResponsiveDrawer = () => {
 </Drawer.Root>`}
               </code>
             </pre>
-            
+          </div>
+          
+          <div className="hidden group-data-[vaul-drawer-direction=top]:block">
+            <Drawer.Handle className="-top-3" />
           </div>
         </Drawer.Content>
       </Drawer.Portal>
